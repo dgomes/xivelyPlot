e7bfb6d43b2a0d559a50c4604d26246e853b81cb
Diogo Gomes <dgomes@ua.pt>
2013-09-19 22:49:11 +0100

    added Current Value

--- js/main.js
+++ js/main.js
@@ -1,65 +1,106 @@
 $(document).ready(function($) {
+		//Settings
+		plotSelector = '#plotArea';
+		currentSelector = '#current';
+		currentLabelSelector = '#current_label';
+
+
 		console.log("XivelyPlot");
 		//Storage
 		storage=$.localStorage
 		console.log("version: "+storage.get('version'));
 
-		$("input").each(function() {
-        	$(this).val(storage.get($(this).attr("name")));
-        });
+		if(storage.get('version')!=null) {
+			//load data from storage
+			$('input[name="apikey"]').val(storage.get('apikey'));
+			$("input").each(function() {
+    	    	$(this).val(storage.get($(this).attr("name")));
+        	});
+			$("select").each(function() {
+    	     	$(this).append($("<option></option>").attr("value",storage.get($(this).attr("name"))).text("Loading..."));
+        	});
+		}
 
+		if($('input[name="apikey"]').val().length) {
+			$('#login').hide();
+			loadXivelyData(plotSelector, currentSelector, currentLabelSelector);
+			$('#conf').hide();
+		} else {
+			$('#settings').hide();
+		}
 
-		function updateDatastream(node, ds) {
-			xively.datastream.list(node.val(), function(e) {
-				newselect=' <div class="controls"> <select name="datastreamid" class="input-large">';
-				for(i = 0; i <e.length; i++) {
-					if(ds.val() == e[i].id)
-						newselect+='<option selected>'+e[i].id+'</option>';
-					else
-						newselect+='<option>'+e[i].id+'</option>';
+		function loadXivelyData(selectorPlot, selectorCurrent, selectorCurrentLabel) {
+			// Set the Xively API key (https://xively.com/users/YOUR_USERNAME/keys)
+			xively.setKey( $('input[name="apikey"]').val() );
+
+			xively.feed.list({user: $('input[name="username"]').val(), content:"summary"}, function(e) {
+				if(e.status == 401) {
+					$('#settings').hide();
+					$('#login').show();
 				}
-				newselect+='</select> </div>';
-				ds.replaceWith(newselect);
+
+				current_feedid = $('select[name="feedid"]').val();
+				$('select[name="feedid"] option').remove();
+				$.each(e.results, function(key, feed) {
+					opt = $("<option></option>").attr("value",feed.id).text(feed.title);
+					if(feed.id == current_feedid)
+						opt.attr('selected','selected');
+					$('select[name="feedid"]').append(opt);
+				});
+
+				// Replace select of Datastream id's
+				function updateDatastream(feed) {
+					xively.datastream.list(feed.val(), function(e) {
+						current_datastreamid = $('select[name="datastreamid"]').val();
+						$('select[name="datastreamid"] option').remove();
+
+						$.each(e, function(key, datastream) {
+							opt = $("<option></option>").attr("value",datastream.id).text(datastream.id);
+							if(datastream.id == current_datastreamid)
+								opt.attr('selected','selected');
+							$('select[name="datastreamid"]').append(opt);
+						});
+					});
+				}
+				updateDatastream($('select[name="feedid"]'));
+
+				$('select[name="feedid"]').change(function() {
+					updateDatastream($(this));
+				});
+			});
+			plot($('select[name="feedid"]').val(), $('select[name="datastreamid"]').val(), selectorPlot);
+			showData($('select[name="feedid"]').val(), $('select[name="datastreamid"]').val(), selectorCurrent, selectorCurrentLabel);
+		}
+
+		function showData(feedID, datastreamID, selector, label) {
+			xively.datastream.get (feedID, datastreamID, function ( datastream ) {
+				console.log(datastream);
+				$(selector).html( parseFloat(('' + (datastream["current_value"]+0.001)).match(/\d*\.\d{2}/)[0])+datastream["unit"]["symbol"]);
+
+				$(label).html(datastream["id"]);
+
+				xively.datastream.subscribe( feedID, datastreamID, function ( event , datastream_updated ) {
+					// Display the current value from the updated datastream
+					$(selector).html( parseFloat(('' + (datastream["current_value"]+0.001)).match(/\d*\.\d{2}/)[0])+datastream["unit"]["symbol"]);
+	    		});
 			});
 		}
-		// Set the Xively API key (https://xively.com/users/YOUR_USERNAME/keys)
-		xively.setKey( $('input[name="apikey"]').val() );
 
-		xively.feed.list({user:"dgomes", content:"summary"}, function(e) {
-			newselect=' <div class="controls"> <select name="feedid" class="input-large">';
-			for(i = 0; i <e.results.length; i++) {
-				if($('input[name="feedid"]').val() == e.results[i].id)
-					newselect+='<option value='+e.results[i].id+' selected>'+e.results[i].title+'</option>';
-				else
-					newselect+='<option value='+e.results[i].id+'>'+e.results[i].title+'</option>';
-			}
-    		newselect+='</select> </div>';
-			$('input[name="feedid"]').replaceWith(newselect);
-			updateDatastream($('select[name="feedid"]'), $('input[name="datastreamid"]'));
-			$('select[name="feedid"]').change(function() {
-				updateDatastream($(this),$('select[name="datastreamid"]'));
-			});
-		});
+		function plot(feedID, datastreamID,selector) {
 
-		// Replace with your own values
-		var feedID        = $('input[name="feedid"]').val(),          // Feed ID (the last number on the URL on the feed page on Xively)
-		datastreamID  = $('input[name="datastreamid"]').val();       // Datastream ID
-		selector      = "#plotArea";   // Your element on the page - takes any valid jQuery selector
+			// Flotr
+	        var data, graph,
+			options = {duration:"1days"};
 
-		// Flotr
-        var container = document.getElementById('plotArea'),
-          data, graph,
-		options = {duration:"24hours"};
-
-		// Get datastream data from Xively
-		xively.datastream.history (feedID, datastreamID, options, function ( datastream ) {
+			// Get datastream data from Xively
+			xively.datastream.history (feedID, datastreamID, options, function ( datastream ) {
           		data = [];
 				for(i=0; i<datastream.datapoints.length; i++) {
 					data.push([new Date(datastream.datapoints[i].at), parseInt(datastream.datapoints[i].value)]);
 				}
 
 				// Draw Graph
-          		graph = Flotr.draw(container, [ { data: data, label: datastreamID, lines: {show: true}, points: {show: true}, mouse: {track: true} } ], {
+          		graph = Flotr.draw($(selector)[0], [ { data: data, label: datastreamID, lines: {show: true}, points: {show: true}, mouse: {track: true} } ], {
 					xaxis : {
 						mode : 'time',
 						labelsAngle: 45
@@ -70,6 +111,40 @@ $(document).ready(function($) {
             		}
           		});
 
+			});
+		}
+
+
+		// User Actions
+		$('#login').submit(function() {
+			if($('input[name="username"]').val().length > 0 && $('input[name="password"]').val().length > 0) {
+				$.ajax({
+					type: "POST",
+				    url: "https://api.xively.com/v2/keys.json",
+					beforeSend: function (xhr){
+						xhr.setRequestHeader('Authorization', "Basic " + btoa($('input[name="username"]').val()+':'+ $('input[name="password"]').val()));
+					},
+					data: JSON.stringify({ "key":{ "label":"test key", "private_access":"false", "permissions":[ { "access_methods":["get"] } ] } }),
+				    dataType: "json",
+					async: false,
+					complete: function(xhr) {
+            	    	if (xhr.readyState == 4) {
+                	    	if (xhr.status == 201) {
+								//parse key which is returned in the Location header...
+								var key = xhr.getResponseHeader('Location').split('/')[5];
+								$('input[name="apikey"]').val(key);
+								storage.set("apikey",key);
+								$('#login').hide();
+								loadXivelyData(plotSelector, currentSelector, currentLabelSelector);
+								$('#settings').show();
+		                    }
+    		            } else {
+        		            alert("NoGood");
+            		    }
+            		}
+				});
+			}
+			return false;
 		});
 
 		$('#settings').submit(function() {
@@ -77,6 +152,7 @@ $(document).ready(function($) {
 			storage.set('version', 0);
 
 			$("input").each(function() {
+				if($(this).attr("name") == "password") return;
 				console.log($(this).attr("name")+" -> "+$(this).val());
 				storage.set($(this).attr("name"),$(this).val());
 			});
@@ -84,8 +160,13 @@ $(document).ready(function($) {
 				console.log($(this).attr("name")+" -> "+$(this).val());
 				storage.set($(this).attr("name"),$(this).val());
 			});
+			plot($('select[name="feedid"]').val(), $('select[name="datastreamid"]').val(), plotSelector);
+			showData($('select[name="feedid"]').val(), $('select[name="datastreamid"]').val(), currentSelector, currentLabelSelector);
+			return false;
+		});
 
-//			return false;
+		$('#confButton').click(function() {
+			$('#conf').toggle("slow");
 		});
 
 })
